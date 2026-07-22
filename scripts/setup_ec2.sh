bfea9cac788c3ea289de08ff0a61b5ca8274dbf3
runo313 <ogheneruno313@gmail.com>
2026-07-21 21:14:58 -0400

    add Initialize conda for script use

--- scripts/setup_ec2.sh
+++ scripts/setup_ec2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source /opt/conda/etc/profile.d/conda.sh
 
 git clone https://github.com/runo313/automated-xray-reporting
 cd automated-xray-reporting
@@ -8,3 +9,5 @@ conda env create -f environment.yml
 conda activate xray-report
 
 python -c "import torch; print(torch.cuda.is_available(), torch.cuda.get_device_name(0))"
+
+echo "Environment ready"
