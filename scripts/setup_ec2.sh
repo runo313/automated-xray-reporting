26423328a6f13820156c1e016345811db3677b1b
runo313 <ogheneruno313@gmail.com>
2026-07-27 19:01:36 -0400

    update dependencies

--- scripts/setup_ec2.sh
+++ scripts/setup_ec2.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
-source /opt/conda/etc/profile.d/conda.sh
 
-git clone https://github.com/runo313/automated-xray-reporting
 cd automated-xray-reporting
 
-conda env create -f environment.yml
-conda activate xray-report
+python3 -m venv xray-env
+source xray-env/bin/activate
 
-python -c "import torch; print(torch.cuda.is_available(), torch.cuda.get_device_name(0))"
+pip install --upgrade pip
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install -r requirements.txt
+
+python3 -c "import torch; print(torch.cuda.is_available(), torch.cuda.get_device_name(0))"
 
 echo "Environment ready"
